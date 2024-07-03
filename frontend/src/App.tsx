be222d6fd3dc9bb3ff0dc6b99bb00cdd1968a159
dm-gilmutdinova <dm.gilmutdinova@gmail.com>
2024-07-03 13:19:16 +0300

    updating button style in btnCategory

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,11 +6,16 @@ import { Paths } from './paths';
 import { Main } from './pages/main/Main';
 
 import './index.css';
+import { Catalog } from './pages/catalog/Catalog';
 
 const router = createBrowserRouter([
   {
     path: Paths.main,
     element: <Main />,
+  },
+  {
+    path: Paths.catalog,
+    element: <Catalog />,
   },
 ]);
 
